0fea68e7ae29f6150af800b0a51907cb59b07537
ErrorOliver <ErrorOliver6@proton.me>
2025-05-17 15:44:04 +0000

    Update constants.ts

--- src/scripts/constants.ts
+++ src/scripts/constants.ts
@@ -1,24 +1,24 @@
-export const DISCORD_INVITE = "https://discord.gg/D9uwnFnqmd";
-export const GITHUB_URL = "https://github.com/Vendicated/Vencord";
-export const SOURCE_CODE = "https://github.com/Vencord/vencord.dev";
+export const DISCORD_INVITE = "https://discord.gg/574gU979yp";
+export const GITHUB_URL = "https://github.com/Scorncord/Scorncord";
+export const SOURCE_CODE = "https://github.com/Scorncord/scorncord.erroroliver.lol";
 export const VENCORD_CLOUD_SOURCE_CODE_URL =
-    "https://github.com/Vencord/Vencloud";
-export const SPONSOR_URL = "https://github.com/sponsors/Vendicated";
+    "https://github.com/Scorncord/Scorncloud";
+export const SPONSOR_URL = "https://github.com/sponsors/Scorncord";
 
 export const DOWNLOAD_BASE =
-    "https://github.com/Vencord/Installer/releases/latest/download/";
+    "https://github.com/Scorncord/Installer/releases/latest/download/";
 
 export const PLUGINS_JSON_URL =
-    "https://raw.githubusercontent.com/Vencord/builds/main/plugins.json";
+    "https://raw.githubusercontent.com/Scorncord/builds/main/plugins.json";
 export const PLUGIN_READMES_JSON_URL =
-    "https://raw.githubusercontent.com/Vencord/builds/main/plugin-readmes.json";
+    "https://raw.githubusercontent.com/Scorncord/builds/main/plugin-readmes.json";
 
 export const FIREFOX_WEBSTORE_URL =
     "https://addons.mozilla.org/en-GB/firefox/addon/vencord-web";
 export const CHROME_WEBSTORE_URL =
     "https://chrome.google.com/webstore/detail/vencord-web/cbghhgpcnddeihccjmnadmkaejncjndb";
 export const USERSCRIPT_URL =
-    "https://raw.githubusercontent.com/Vencord/builds/main/Vencord.user.js";
+    "https://raw.githubusercontent.com/Scorncord/builds/main/Scorncord.user.js";
 
 export const IS_SERVER = import.meta.env.SSR;
 
